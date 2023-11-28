5a93f5194252a8e4bcf407f6317705dd27c14a32
Zachary De Aguiar <deaguiarzachary@gmail.com>
2023-11-27 22:52:01 -0500

    Added Iteration + Output to CSV

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
 import requests
 import json
+import csv
+import time
 
 def Get_Matches():
-    url = "https://www.fotmob.com/api/leagues?id=130&ccode3=USA_FL&season=2022"
-
+    url = 'https://www.fotmob.com/api/leagues?id=130&ccode3=USA_FL&season=2022'
     payload = {}
     headers = {}
 
-    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+    matchData = json.loads(requests.request("GET", url, headers=headers, data=payload).text)["matches"]["allMatches"]
 
-    # print(response["matches"]["allMatches"][0])
+    with open('matches.csv', 'w', newline='', encoding='utf-8') as file:
+        fieldnames = ['Match ID', 'Date', 'Stadium', 'City', 'Country', 'Latitude', 'Longitude']
+        for i in range(1, 23):
+            fieldnames.extend([f'Player{i} Rating'])
+        
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
 
-    matchId = response["matches"]["allMatches"][0]["id"]
-    matchUrl = f"https://www.fotmob.com/api/matchDetails?matchId={matchId}"
-    matchResponse = json.loads(requests.request("GET", matchUrl, headers=headers, data=payload).text)
+        for match in matchData:
+            matchId = match["id"]
+            matchUrl = f"https://www.fotmob.com/api/matchDetails?matchId={matchId}"
+            matchResponse = json.loads(requests.request("GET", matchUrl, headers=headers, data=payload).text)
 
-    # first index is team # (0 or 1), second index is player #, third index is always 0, fourth index is always 0
-    team = 0
-    player = 4
-    print(matchResponse["content"]["lineup"]["lineup"][team]["optaLineup"]["players"][player][0]["stats"][0]["stats"]["FotMob rating"]["value"])
+            stadium = matchResponse['content']['matchFacts']['infoBox']['Stadium']
+            date = matchResponse["general"]["matchTimeUTC"]
+
+            row_data = {
+                'Match ID': matchId,
+                'Date': date,
+                'Stadium': stadium['name'],
+                'City': stadium['city'],
+                'Country': stadium['country'],
+                'Latitude': stadium['lat'],
+                'Longitude': stadium['long']
+            }
+
+            player_count = 1
+            for team in range(2):
+                teamName = matchResponse['content']['lineup']['lineup'][team]['teamName']
+
+                try:
+                    for player in matchResponse['content']['lineup']['lineup'][team]['optaLineup']['players']:
+                        for obj in player:
+                            playerRating = obj["rating"]["num"]
+
+                            row_data[f'Player{player_count} Rating'] = playerRating
+
+                            player_count += 1
+                            if player_count > 22:
+                                break
+
+                except Exception as e:
+                    print(f"Error finding lineup")
+
+            writer.writerow(row_data)
+
+            time.sleep(2.5)
+
 Get_Matches()
