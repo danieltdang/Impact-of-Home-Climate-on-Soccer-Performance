dd387750fdd1fdf3dfbc1cd4f3353ba427a35fdb
danieltdang <dtd21b@fsu.edu>
2023-11-27 21:47:14 -0500

    Implemented location, team, player name and rating

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ def Get_Matches():
     # print(response["matches"]["allMatches"][0])
 
     matchId = response["matches"]["allMatches"][0]["id"]
+    
     matchUrl = f"https://www.fotmob.com/api/matchDetails?matchId={matchId}"
     matchResponse = json.loads(requests.request("GET", matchUrl, headers=headers, data=payload).text)
 
-    # first index is team # (0 or 1), second index is player #, third index is always 0, fourth index is always 0
+    # variables for the match
     team = 0
     player = 4
-    print(matchResponse["content"]["lineup"]["lineup"][team]["optaLineup"]["players"][player][0]["stats"][0]["stats"]["FotMob rating"]["value"])
+    
+    # Dictionary with keys: name, city, country, lat, long
+    stadium = matchResponse["content"]["matchFacts"]["infoBox"]["Stadium"]
+    teamName = matchResponse["content"]["lineup"]["lineup"][team]["teamName"]
+    
+    # keep the indices 0 and 0 at the end for player name and rating
+    playerName = matchResponse["content"]["lineup"]["lineup"][team]["optaLineup"]["players"][player][0]["name"]["fullName"]
+    playerRating = matchResponse["content"]["lineup"]["lineup"][team]["optaLineup"]["players"][player][0]["stats"][0]["stats"]["FotMob rating"]["value"]
+    
+    print(f"Match ID: {matchId}")
+    print(f"{stadium['name']}: {stadium['city']}, {stadium['country']} ({stadium['lat']}, {stadium['long']})")
+    print(f"{teamName} - {playerName}:  {playerRating}")
+    
 Get_Matches()
