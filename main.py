3e327c5442e9477593688c50017eec10c3a8152c
danieltdang <dtd21b@fsu.edu>
2023-11-27 21:58:13 -0500

    Added some comments

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ def Get_Matches():
     # Dictionary with keys: name, city, country, lat, long
     stadium = matchResponse['content']['matchFacts']['infoBox']['Stadium']
     
+    # Prints match ID and stadium info
     print(f"Match ID: {matchId}")
     print(f"{stadium['name']}: {stadium['city']}, {stadium['country']} ({stadium['lat']}, {stadium['long']})")
     
+    # Loops through both teams
     for team in range(2):
         teamName = matchResponse['content']['lineup']['lineup'][team]['teamName']
         print(teamName)
         
+        # Loops through players on each team
         for player in matchResponse['content']['lineup']['lineup'][team]['optaLineup']['players']:
             playerName = player[0]['name']['fullName']
             playerRating = player[0]['stats'][0]['stats']['FotMob rating']['value']
