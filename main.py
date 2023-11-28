7db9985fd385ff2032f19a6dfee917d93d83b724
danieltdang <dtd21b@fsu.edu>
2023-11-27 21:06:38 -0500

    Added index variables and clearer comment

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def Get_Matches():
     matchUrl = f"https://www.fotmob.com/api/matchDetails?matchId={matchId}"
     matchResponse = json.loads(requests.request("GET", matchUrl, headers=headers, data=payload).text)
 
-    # first index is lineup #, second index is player #, third index is always 0, fourth index is always 0
-    # prints FotMob rating
-    print(matchResponse["content"]["lineup"]["lineup"][0]["optaLineup"]["players"][0][0]["stats"][0]["stats"]["FotMob rating"]["value"])
+    # first index is team # (0 or 1), second index is player #, third index is always 0, fourth index is always 0
+    team = 0
+    player = 4
+    print(matchResponse["content"]["lineup"]["lineup"][team]["optaLineup"]["players"][player][0]["stats"][0]["stats"]["FotMob rating"]["value"])
 Get_Matches()
