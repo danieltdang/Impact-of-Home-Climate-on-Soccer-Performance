9459bdc7854c37cdd5a8149be756a101a63903cd
Zachary De Aguiar <deaguiarzachary@gmail.com>
2023-11-29 14:57:51 -0500

    Added Player Rating Avgs per "Position"

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import csv
 import time
 
+TEAM_ROLES = ['Keeper', 'Defender', 'Midfielder', 'Attacker']
+
 def Get_Matches():
     df = pvcz.get_pvcz_data()
     
@@ -18,9 +20,12 @@ def Get_Matches():
     
     print(f"Processing {matchCount} matches...")
     with open('matches.csv', 'w', newline='', encoding='utf-8') as file:
-        fieldnames = ['Match ID', 'Date', 'Stadium', 'City', 'Country', 'Latitude', 'Longitude', 'Koppen Climate', 'Team 1', 'Team 2']
-        for i in range(1, 23):
-            fieldnames.extend([f'Player{i} Rating'])
+        fieldnames = ['Match ID', 'Date', 'Stadium', 'City', 'Country', 'Latitude', 'Longitude', 'Koppen Climate', 'Team 1', 'Team 2', 'HomeTeam Avg Rating', 'AwayTeam Avg Rating']
+        for role in TEAM_ROLES:
+            fieldnames.append(f'HomeTeam {role} Avg Rating')
+        
+        for role in TEAM_ROLES:
+            fieldnames.append(f'AwayTeam {role} Avg Rating')
         
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
@@ -49,30 +54,43 @@ def Get_Matches():
                 'Team 2': matchResponse['content']['lineup']['lineup'][1]["teamName"]
             }
 
-            player_count = 1
             for team in range(2):
+                playerRating = [0,0,0,0]
+                playerCount = [0,0,0,0]
                 try:
                     for roles in matchResponse['content']['lineup']['lineup'][team]['players']:
                         for player in roles:
+
                             try:
-                                # playerRating = player["rating"]["num"]
-                                playerRating = player['stats'][0]['stats']['FotMob rating']['value']
-
-                                row_data[f'Player{player_count} Rating'] = playerRating
-
-                                player_count += 1
-                                if player_count > 22:
-                                    break
+                                if player["position"] == 'Keeper':
+                                    playerRating[0] += player['stats'][0]['stats']['FotMob rating']['value']
+                                    playerCount[0] += 1
+                                elif player["position"] == 'Defender':
+                                    playerRating[1] += player['stats'][0]['stats']['FotMob rating']['value']
+                                    playerCount[1] += 1
+                                elif player["position"] == 'Midfielder':
+                                    playerRating[2] += player['stats'][0]['stats']['FotMob rating']['value']
+                                    playerCount[2] += 1
+                                elif player["position"] == 'Attacker':
+                                    playerRating[3] += player['stats'][0]['stats']['FotMob rating']['value']
+                                    playerCount[3] += 1 
+                                
                             except Exception as e:
                                 errorCount += 1
                                 print(f"Match {i} [{matchId}] - Team {team + 1} Error finding player rating: {e}")
                                 continue
                     
-                    # print(f"Match {i} [{matchId}] - Team {team + 1} Successfully written to file")
+                    #print(f"Match {i} [{matchId}] - Team {team + 1} Successfully written to file")
 
                 except Exception as e:
                     errorCount += 1
                     print(f"Match {i} [{matchId}] - Team {team + 1} Error finding lineup: {e}")
+
+                for i in range(0, 4):
+                    if team == 0: 
+                        row_data[f'HomeTeam {TEAM_ROLES[i]} Avg Rating'] = round(playerRating[i] / playerCount[i],2)
+                    else:
+                        row_data[f'AwayTeam {TEAM_ROLES[i]} Avg Rating'] = round(playerRating[i] / playerCount[i],2)
 
             writer.writerow(row_data)
             print(f"Match {i} [{matchId}] - Successfully written to file")
