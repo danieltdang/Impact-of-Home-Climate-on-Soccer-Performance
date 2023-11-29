bcf6daf32a6f3173b46526e9e827b9bba437d07c
danieltdang <dtd21b@fsu.edu>
2023-11-29 09:48:22 -0500

    Added logging of total matches to process

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def Get_Matches():
     # Stored counts for printing after finished processing all matches
     matchCount, errorCount = len(matchData), 0
     
+    print(f"Processing {matchCount} matches...")
     with open('matches.csv', 'w', newline='', encoding='utf-8') as file:
         fieldnames = ['Match ID', 'Date', 'Stadium', 'City', 'Country', 'Latitude', 'Longitude', 'Koppen Climate', 'Team 1', 'Team 2']
         for i in range(1, 23):
