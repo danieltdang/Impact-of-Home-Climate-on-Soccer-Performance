4a4045955214fd4bc5d11b6c115012660b8da566
danieltdang <dtd21b@fsu.edu>
2023-12-06 20:55:19 -0500

    Moved ANOVA in main.py to anova.py

--- modules/anova.py
+++ modules/anova.py
@@ -1,5 +1,34 @@
+import numpy as np 
+import pandas as pd
+import statsmodels.api as sm 
+from statsmodels.formula.api import ols
+
 def anova():
-    pass
+    print("--------------------------------------------------------")
+    print("Starting the ANOVA tests.")
+    # Load data
+    print("Loading data from matches.csv...")
+    df = pd.read_csv('matches.csv')
+
+    print("Setting the features and target variables...")
+    df.columns = df.columns.str.replace('Elevation (meters)', 'Elevation')
+    df.columns = df.columns.str.replace('Temperature (c)', 'Temperature')
+    df.columns = df.columns.str.replace('Humidity (g/kg)', 'Humidity')
+    df.columns = df.columns.str.replace(' ', '_')
+
+    # ANOVA
+    print("Processing ANOVA...")
+    model = ols('Away_Avg_Rating ~ Elevation + Temperature + Humidity', data=df).fit()
+    anova_result = sm.stats.anova_lm(model, typ=2)
+
+    # Printing results
+    print("----------------------------")
+    print("ANOVA results:\n")
+    print(anova_result)
+    print("----------------------------\n")
+
+    print("Finished the ANOVA tests.")
+    print("--------------------------------------------------------")
 
 if __name__ == "__main__":
     anova()
