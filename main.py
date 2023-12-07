25f6451c5418c5038208538f4aa88de48baa556a
danieltdang <dtd21b@fsu.edu>
2023-12-06 21:52:04 -0500

    Commented out retrieval matches

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from modules import matches, anova as a, multiple_linear_regression as mlr, random_forest as rf
 
- matches.get_matches()
+# matches.get_matches()
 rf.random_forest()
 mlr.multiple_linear_regression()
 a.anova()
