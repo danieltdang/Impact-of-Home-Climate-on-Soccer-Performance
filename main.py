d7d299936c12d44f931b433f340149f378593371
danieltdang <dtd21b@fsu.edu>
2023-12-08 15:42:30 -0500

    Added time stamps
    Co-authored-by: zdeag <zdeag@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from modules import matches, anova as a, multiple_linear_regression as mlr, random_forest as rf
+import time
 
-# matches.get_matches()
+"""start_time = time.time()
+matches.get_matches()
+end_time = time.time()
+print(f"get_matches() took {end_time - start_time} seconds.")"""
+
+start_time = time.time()
 rf.random_forest()
+end_time = time.time()
+print(f"Random Forest took {round(end_time - start_time, 4)} seconds.")
+
+start_time = time.time()
 mlr.multiple_linear_regression()
+end_time = time.time()
+print(f"Multiple Linear Regression took {round(end_time - start_time, 4)} seconds.")
+
+start_time = time.time()
 a.anova()
+end_time = time.time()
+print(f"ANOVA took {round(end_time - start_time, 4)} seconds.")
